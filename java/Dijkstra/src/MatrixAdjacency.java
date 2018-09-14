cc90ec01d64c04fab5b9a30e1d34de136de77ace
chrislucas <christoffer.luccas@gmail.com>
2018-09-14 15:55:39 -0300

    estudos

--- java/Dijkstra/src/MatrixAdjacency.java
+++ java/Dijkstra/src/MatrixAdjacency.java
@@ -1,6 +1,35 @@
 public class MatrixAdjacency {
 
 
+    private static int [][] matrix;
+    private static boolean [] visiteds;
+    private static int [] distances;
+
+    private static void minDistance() {
+
+    }
+
+    // All pairs shortest path
+    private static void alsp() {
+
+    }
+
+    private static void test1() {
+        //add();
+    }
+
+    private static void add(int a, int b) {
+        matrix[a][b] = 1;
+    }
+
+    public static void run(int vertices) {
+        matrix = new int[vertices][vertices];
+        visiteds = new boolean[vertices];
+        distances = new int[vertices];
+        test1();
+    }
+
+
     public static void main(String[] args) {
 
     }

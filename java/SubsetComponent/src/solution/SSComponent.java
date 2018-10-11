923a978fe3aa9602a26fe8f326f02964ddd17019
chrislucas <christoffer.luccas@gmail.com>
2018-10-11 17:20:47 -0300

    um exercicio para resolver

---
+++ java/SubsetComponent/src/solution/SSComponent.java
@@ -0,0 +1,14 @@
+package solution;
+
+
+/**
+ * https://www.hackerrank.com/challenges/subset-component/problem?utm_campaign=challenge-recommendation&utm_medium=email&utm_source=24-hour-campaign
+ * https://en.wikipedia.org/wiki/Connected_component_%28graph_theory%29
+ * */
+
+public class SSComponent {
+
+    public static void main(String[] args) {
+
+    }
+}
